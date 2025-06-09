4ff9e0ee4ef8375e3208961ef4c1010c13a52115
Lucas Pollo <lucasrefinskipollo@gmail.com>
2025-06-09 19:54:27 -0300

    comeco do codigo

---
+++ src/atividade.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class atividade {
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Bem vindo ao sistema de avaliacoes de alunos!");
+        System.out.println("Digite seu nome: ");
+        String nome = ler.next();
+
+
+        System.out.println("Digite seu ra: ");
+        int ra = ler.nextInt();
+
+
+        System.out.println("Digite sua turma: ");
+        String turmaEscolhida = ler.next();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
