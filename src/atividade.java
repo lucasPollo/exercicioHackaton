b7596adc0f2bafb672f6f615fef1b42e1658065b
luislrda28 <luisotaviorsilva@hotmail.com>
2025-06-09 21:25:18 -0300

    quase pronto

--- src/atividade.java
+++ src/atividade.java
@@ -2,41 +2,47 @@ import java.util.Scanner;
 
 public class Atividade {
     public static Scanner ler = new Scanner(System.in);
+    
+    public static String[] turmas = {
+        "ADS", "Sistemas", "Agronomia", "Administração", "Arquitetura",
+        "Biomedicina", "Farmácia", "Contábeis", "Direito", "Eng. Civil",
+        "Veterinária", "Psicologia", "Psicopedagogia"
+    };
 
-    // Método para decidir a turma (exemplo simples)
     public static int decideTurma() {
-        String[] turmas = {"ADS","Sistemas","Agronomia","Administração","Arquitetura","Biomedicina","Farmacia","Contabeis","Direito","Eng.Civil","Veterinaria","Psicologia","Psicopedagogia"};
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.println("Escolha a turma:");
+
+        System.out.println("\n========== Escolha sua Turma ==========");
         for (int i = 0; i < turmas.length; i++) {
-            System.out.println((i + 1) + " - " + turmas[i]);
+            System.out.printf(" %2d) %-15s\n", (i + 1), turmas[i]);
         }
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\n");
-        int decisao = sc.nextInt();
+        System.out.println("========================================");
+        System.out.print("Digite o número correspondente à sua turma: ");
+
+        int decisao = ler.nextInt();
         return decisao;
     }
 
     public static void exibirMenuFeedback() {
+        int nota = 0;
 
-        int turmaescolhida = decideTurma();
-
-         System.out.println("\nAvalie o professor do seu programa e ajude-o a melhor da melhor maneira possível!\n");
-        System.out.println("\nEm que pontos você acha que seu professor é muito bom?\n");
+        System.out.println("\nAvalie o professor do seu programa e ajude-o a melhor da melhor maneira possível!\n");
+        System.out.println("\nEm que pontos você acha que seu professor é muito bom?");
+        ler.nextLine();
         String pontosFortes = ler.nextLine();
         System.out.println("\nEm que pontos você acha que seu professor pode melhorar?\n");
         String pontosFracos = ler.nextLine();
         System.out.println("\nOtimo! Obrigado por nos ajudar a melhorar!");
-        System.out.println("\nPor favor deixe uma nota de 0 a 10 para o professor:\n");
-        int nota = ler.nextInt();
 
+        boolean notaInvalida = true;
         do {
             System.out.println("Por favor, insira uma nota de 0 a 10:");
             nota = ler.nextInt();
-            if (nota <= 0 && nota >= 10) {
+            if (nota < 0 || nota > 10) {
                 System.out.println("Nota inválida! Por favor, insira uma nota entre 0 e 10.");
+            }else{
+                notaInvalida = false;
             }
-        } while (nota <= 0 && nota >= 10);
+        } while (notaInvalida);
 
         System.out.println("Pontos fortes: " + pontosFortes);
         System.out.println("Pontos a melhorar: " + pontosFracos);
@@ -46,14 +52,22 @@ public class Atividade {
     public static void main(String[] args) {
 
         System.out.println("Bem vindo ao sistema de avaliacoes de alunos!");
-        System.out.println("Digite seu nome: ");
+        System.out.print("Digite seu nome: ");
         String nome = ler.next();
 
 
-        System.out.println("Digite seu ra: ");
+        System.out.print("Digite seu ra: ");
         int ra = ler.nextInt();
 
-        decideTurma();
+        int turmaEscolhida = decideTurma();
+  
+        String turmaSelecionada = turmas[turmaEscolhida - 1];
+
+        turmaz.limparTerminal();
+        System.out.println("\n");
+        System.out.println("Bem-vindo! "+ nome);
+        System.out.println("Sua turma escolhida foi: " + turmaSelecionada);
+
         exibirMenuFeedback();
 
 
---
+++ src/turmaz.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class turmaz {
+    public static void limparTerminal() {
+    for (int i = 0; i < 50; i++) {
+        System.out.println();
+    }   
+    }
+
+}
+    
+
