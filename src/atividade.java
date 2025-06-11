f9c5bb69bd81f323504e6f0237eb24bfdcf3f762
Juca <joaoschneider276@gmail.com>
2025-06-11 20:48:54 -0300

    correção de texto e adição de loop de escolha de turma

--- src/atividade.java
+++ src/atividade.java
@@ -26,11 +26,11 @@ public class atividade {
     public static void exibirMenuFeedback() {
         int nota = 0;
 
-        System.out.println("\nAvalie o professor do seu programa e ajude-o a melhor da melhor maneira possível!\n");
-        System.out.println("\nEm que pontos você acha que seu professor é muito bom?");
+        System.out.println("\nAvalie o professor do seu programa e ajude-o a melhor da melhor maneira possível!");
+        System.out.println("\nQuais você que são os pontos fortes do seu professor?");
         ler.nextLine();
         String pontosFortes = ler.nextLine();
-        System.out.println("\nEm que pontos você acha que seu professor pode melhorar?\n");
+        System.out.println("\nEm que pontos você acha que seu professor pode melhorar?");
         String pontosFracos = ler.nextLine();
         System.out.println("\nOtimo! Obrigado por nos ajudar a melhorar!");
 
@@ -39,15 +39,18 @@ public class atividade {
             System.out.println("Por favor, insira uma nota de 0 a 10:");
             nota = ler.nextInt();
             if (nota < 0 || nota > 10) {
-                System.out.println("Nota inválida! Por favor, insira uma nota entre 0 e 10.");
+                System.out.println("Nota inválida!");
             }else{
                 notaInvalida = false;
             }
         } while (notaInvalida);
 
+        System.out.println("Obrigado pela sua avaliação!");
+        System.out.println("========================================");
         System.out.println("Pontos fortes: " + pontosFortes);
         System.out.println("Pontos a melhorar: " + pontosFracos);
         System.out.println("Nota dada: " + nota);
+        System.out.println("========================================");
 
     }
 
@@ -95,10 +98,42 @@ for(int i =0; i < professoresCursoAds.length; i++) {
 
         exibirMenuFeedback();
 
+        boolean continuar = true;
 
-        mostrarProfessores();
+        while (continuar) {
+            System.out.println("\nDeseja avaliar outro corpo docente? (S/N)");
+            String resposta = ler.next();
+            if (resposta.equalsIgnoreCase("S")) {
+            int novaTurmaEscolhida = decideTurma();
+            String novaTurmaSelecionada = turmas[novaTurmaEscolhida - 1];
 
+            turmaz.limparTerminal();
+            System.out.println("\n");
+            System.out.println("Bem-vindo! " + nome);
+            System.out.println("Sua turma escolhida foi: " + novaTurmaSelecionada + "\n");
 
+            switch (novaTurmaSelecionada) {
+                case "ADS":
+                String[] professores = professoresCursoAds;
+                System.out.println("\nSeus professores são:");
+                for (int i = 0; i < professoresCursoAds.length; i++) {
+                    System.out.printf(" %2d) %-15s\n", (i + 1), professores[i]);
+                }
+                Scanner av = new Scanner(System.in);
+                System.out.println("Que professor você gostaria de avaliar? (1-" + professores.length + ")");
+                int escolhaAvaliar = av.nextInt();
+                break;
+            }
+
+            exibirMenuFeedback();
+            } else if (resposta.equalsIgnoreCase("N")) {
+            System.out.println("\n");
+            System.out.println("Obrigado por usar o sistema de avaliações, " + nome + "!");
+            System.out.println("Esperamos que tenha sido uma experiência positiva.");
+            System.out.println("Até a próxima!");
+            continuar = false;
+            }
+        }
 
 
 
